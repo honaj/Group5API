d4b2b2a818a8109ed48999e95aba34303c344a8d
Johan Hellgren <honajnahoj@gmail.com>
2018-02-22 17:56:53 +0100

    Fixed more stuff

--- Demo/script2.js
+++ Demo/script2.js
@@ -1,33 +1,33 @@
-// grab elements to play with
-var heading = document.getElementsByTagName('H1')[0];
-var button = document.getElementsByTagName('DIV')[0];                                            
+// // grab elements to play with
+// var heading = document.getElementsByTagName('H1')[0];
+// var button = document.getElementsByTagName('DIV')[0];                                            
 
-// let's split the heading text by character
-var chars = heading.innerText.split('');
+// // let's split the heading text by character
+// var chars = heading.innerText.split('');
 
-// wrap the splits into spans and build an object with these spans 
-heading.innerHTML = '<span>' + chars.join('</span><span>') + '</span>';
-var charsObject = heading.getElementsByTagName('SPAN'), l = charsObject.length;
+// // wrap the splits into spans and build an object with these spans 
+// heading.innerHTML = '<span>' + chars.join('</span><span>') + '</span>';
+// var charsObject = heading.getElementsByTagName('SPAN'), l = charsObject.length;
 
 
-// built the tween objects
-var tween1 = KUTE.fromTo(
-    button, 
-    {width:150, opacity:0, height: 70, lineHeight:70, fontSize: 40}, 
-    {width: 100, opacity:1, height: 35, lineHeight:35, fontSize: 20});
+// // built the tween objects
+// var tween1 = KUTE.fromTo(
+//     button, 
+//     {width:150, opacity:0, height: 70, lineHeight:70, fontSize: 40}, 
+//     {width: 100, opacity:1, height: 35, lineHeight:35, fontSize: 20});
 
-for (var i=0; i<l; i++){    
-    var fn = i === l-1 ? startButtonAnimation : null;
-    var elementToAnimate = charsObject[i];
-    var delay = 250*i;
+// for (var i=0; i<l; i++){    
+//     var fn = i === l-1 ? startButtonAnimation : null;
+//     var elementToAnimate = charsObject[i];
+//     var delay = 250*i;
     
-    KUTE.fromTo(
-        elementToAnimate, 
-        {opacity:0, height: 50, fontSize:80, letterSpacing: 20}, 
-        {opacity:1, height: 35, fontSize:50, letterSpacing: 0}, 
-        {complete: fn, delay: delay, duration: 500, easing: 'easingCubicOut'}).start()
+//     KUTE.fromTo(
+//         elementToAnimate, 
+//         {opacity:0, height: 50, fontSize:80, letterSpacing: 20}, 
+//         {opacity:1, height: 35, fontSize:50, letterSpacing: 0}, 
+//         {complete: fn, delay: delay, duration: 500, easing: 'easingCubicOut'}).start()
 
-}
+// }
 
 function startButtonAnimation(){
     tween1.start()
