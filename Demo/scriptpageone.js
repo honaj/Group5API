e569bf73157be5ff88a48a482202cfca924b5c49
Polskig <36158798+Polskig@users.noreply.github.com>
2018-02-22 17:15:46 +0100

    first page

--- Demo/scriptpageone.js
+++ Demo/scriptpageone.js
@@ -9,6 +9,23 @@ var chars = heading.innerText.split('');
 heading.innerHTML = '<span>' + chars.join('</span><span>') + '</span>';
 var charsObject = heading.getElementsByTagName('SPAN'), l = charsObject.length;
 
+var tween1 = KUTE.to(heading, {color: '#9C27B0'}).start();
+var tween2 = KUTE.to(heading, {color: '#069'});
+var tween3 = KUTE.to(heading, {color: '#069'});
+var tween4 = KUTE.to(heading, {color: '#0000ff'});
+var tween5 = KUTE.to(heading, {color: '#33cc33'} );
+var tween6 = KUTE.to(heading, {color: '#ffff00'});
+var tween7 = KUTE.to(heading, {color: '#ff1a1a'});
+var tween8 = KUTE.to(heading, {color: '#00ccff'});
+var tween9 = KUTE.to(heading, {color: '#9C27B0'},{duration: 1500});
+
+tween1.chain(tween2);
+tween2.chain(tween3);
+tween3.chain(tween4);
+tween4.chain(tween5);
+tween5.chain(tween6,tween9);
+tween6.chain(tween7);
+tween7.chain(tween8);
 
 // built the tween objects
 var tween1 = KUTE.fromTo(
@@ -32,3 +49,4 @@ for (var i=0; i<l; i++){
 function startButtonAnimation(){
     tween1.start()
 }
+
